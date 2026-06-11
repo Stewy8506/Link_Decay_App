cf8252eb02eaa3775c6014d017691e08395f05d8
Anuvab Das <dasanuvab38@gmail.com>
2026-06-11 22:36:26 +0530

    bugfix again.

--- android/app/src/main/kotlin/com/linkshelf/decay/MainActivity.kt
+++ android/app/src/main/kotlin/com/linkshelf/decay/MainActivity.kt
@@ -1,5 +1,15 @@
 package com.linkshelf.decay
 
+import android.content.Intent
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+    override fun onPause() {
+        super.onPause()
+        intent?.let {
+            if (it.action?.startsWith("android.intent.action.SEND") == true) {
+                it.action = Intent.ACTION_MAIN
+            }
+        }
+    }
+}
